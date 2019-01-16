a9054ca93f296118558e2d15f653d7abe9a9f122
SeJin12 <tpwls8122@naver.com>
2019-01-16 16:01:58 +0900

    etc clear, solve 2004

--- src/ETC/etc2004.java
+++ src/ETC/etc2004.java
@@ -10,25 +10,24 @@ public class etc2004 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] tmp = br.readLine().split(" ");
-		int n = Integer.parseInt(tmp[0]);
-		int m = Integer.parseInt(tmp[1]);
+		long n = Long.parseLong(tmp[0]);
+		long m = Long.parseLong(tmp[1]);
 
-		int cnt2 = 0, cnt5 = 0;
+		long cnt2 = 0, cnt5 = 0;
 
-		for (int i = 2; i <= n; i *= 2) {
+		for (long i = 2; i <= n; i *= 2) {
 			cnt2 += n / i;
 			if (i <= m) // i가 m이 될때까지 분자 n!의 2의 개수와 같기 때문에 m될때까지 같이 빼준다.
-				cnt2 -= n / i;
+				cnt2 -= m / i;
 			if (i <= (n - m)) // 마찬가지, 그 이후에는 안빼준다.
-				cnt2 -= n / i;
+				cnt2 -= (n - m) / i;
 		}
-
-		for (int i = 5; i <= n; i *= 5) {
+		for (long i = 5; i <= n; i *= 5) {
 			cnt5 += n / i;
 			if (i <= m)
-				cnt5 -= n / i;
+				cnt5 -= m / i;
 			if (i <= n - m)
-				cnt5 -= n / i;
+				cnt5 -= (n - m) / i;
 		}
 
 		System.out.println(Math.min(cnt2, cnt5)); // 0의 개수 출력
